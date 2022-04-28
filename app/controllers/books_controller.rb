7132376ebc32162d6846fd2bcfa94de65bc3ec2f
EC2 Default User <ec2-user@ip-172-31-38-211.ec2.internal>
2022-04-28 04:00:33 +0000

    [Upd] Apply Validation (Success Message)

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -19,18 +19,26 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    
+    if @book.save
+    #詳細画面へリダイレクト
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book.id)
+    end
   end
 
   def create
     #データを受け取り新規登録するためのインスタンス作成
-    book = Book.new(book_params)
+    @book = Book.new(book_params)
     #データをデータベースに保存するためのsaveメソッド実行
-    book.save
-    #一覧画面へリダイレクト
-    redirect_to '/books'
+    if @book.save
+    #詳細画面へリダイレクト
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+      #一覧画面を表示、エラーメッセージを出したい
+    end
   end
 
   def destroy
