37e5e6b99d472bfd7c243008762970c7c9cdbe30
EC2 Default User <ec2-user@ip-172-31-38-211.ec2.internal>
2022-04-30 11:11:45 +0000

    [Upd] Generate routing using resources method

--- config/routes.rb
+++ config/routes.rb
@@ -3,17 +3,12 @@ Rails.application.routes.draw do
   get 'homes/top'
   root to: 'homes#top'
   
-  get 'books/index'
+  resources :books
+  
   get 'books' => 'books#index',as:'index_book'
-  
-  #routing追記
-  
   get 'books' => 'books#new'
-  post 'books' => 'books#create'
-  get 'books/show/:id'=> 'books#show',as: 'book'
-  get 'books/:id/edit' => 'books#edit',as: 'edit_book'
-  patch 'books/:id' => 'books#update', as: 'update_book'
   delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+  patch 'books/:id' => 'books#update', as: 'update_book'
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
